c8fef921aca959d175b728fbba47765f996da2ea
Ric-De <130886374+Ric-De@users.noreply.github.com>
2023-09-13 10:02:31 +0200

    Update sprint1_tema0.php
    Seconda line de codigo en php. Prueba de execucion de commit y push.

--- sprint1_tema0.php
+++ sprint1_tema0.php
@@ -1,3 +1,5 @@
 <?php
 echo "Esta es una linea de codigo en php para prueba del uso del github";
+
+echo "Esta es una seconda linea de codigo en php prueba de modicaciones en el archivo usando el sistema de controlo de versionamento con el Github";
 ?>
