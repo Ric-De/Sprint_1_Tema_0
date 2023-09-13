7e59a616b22a9fada6881476fe0c8220e14af92e
Ric-De <ricdellazari@gmail.com>
2023-09-13 10:07:10 +0200

    Update sprint1_tema0.php
    Tercera line de codigo anadida al archivo para prueba de uso correcto del commit, push y pull requests en el GitHub

--- sprint1_tema0.php
+++ sprint1_tema0.php
@@ -2,4 +2,6 @@
 echo "Esta es una linea de codigo en php para prueba del uso del github";
 
 echo "Esta es una seconda linea de codigo en php prueba de modicaciones en el archivo usando el sistema de controlo de versionamento con el Github";
+
+echo "Esta es una tercera linea de codigo en php para prueba del uso del github";
 ?>
